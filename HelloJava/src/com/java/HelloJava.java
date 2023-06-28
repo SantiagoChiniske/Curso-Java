aa348dae48be9e7999d72b491bda1644cf335d46
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-06-27 22:17:57 -0300

    Tipos de Dados e Variáveis

--- HelloJava/src/com/java/HelloJava.java
+++ HelloJava/src/com/java/HelloJava.java
@@ -4,6 +4,17 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int idade = 10;
+		double preco = 10.0;
+		char sexo ='M';
+		boolean temFilhos = true;
+		
+		System.out.println(idade);
+		System.out.println(preco);
+		System.out.println(sexo);
+		System.out.println(temFilhos);
+
 		System.out.println("Hello Java");
 	}
 
