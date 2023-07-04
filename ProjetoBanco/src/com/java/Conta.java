034c827e438805a311f0463df55c1b34f3da340a
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-07-03 22:34:14 -0300

    Classe,Métodos,Atributos, Contrutores

--- ProjetoBanco/src/com/java/Conta.java
+++ ProjetoBanco/src/com/java/Conta.java
@@ -2,4 +2,62 @@ package com.java;
 
 public class Conta {
 
+	//variaveis
+	private int numero;
+	private double saldo;
+	int agencia;
+	
+	
+	public Conta () {
+		
+	}
+	
+	public Conta (int numero) {
+		this.numero = numero;
+		this.saldo = 0;
+	}
+	
+	public Conta(int numero, double saldo) {
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	
+	
+	public Conta (double saldo) {
+		this.saldo = saldo;
+	}
+	
+	//Herdar de outra clase
+	Cliente cliente = new Cliente();
+	
+	//Metódos
+	public double recuperarSaldo() {
+		return saldo;
+	}
+	
+	public void depositar (double valor) {
+		saldo = saldo + valor;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public void retirar(double valor) {
+		saldo = saldo-valor;
+	}
+	
+	public void retirar (double valor, double taxa) {
+		saldo = saldo - valor - taxa;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	
 }
--- ProjetoBanco/src/com/java/TesteConta.java
+++ ProjetoBanco/src/com/java/TesteConta.java
@@ -4,9 +4,46 @@ public class TesteConta {
 	
 	public static void main(String[] args) {
 		
-		Conta contaCorrente = new Conta();
-		Conta contaPoupanca = new Conta();
-		Conta contaInvestimento = new Conta();
+		
+		Conta cc = new Conta(1000);
+		
+		
+		
+		cc.setNumero(1000);
+		
+		System.out.println(cc.recuperarSaldo());
+		
+		
+		
+		
+		
+		
+		
+		//
+		//Conta contaCorrente = new Conta();
+		//Conta contaPoupanca = new Conta();
+		//Conta contaInvestimento = new Conta();
+		
+		//contaCorrente.numero = 11;
+		//contaCorrente.saldo = 20.5;
+		//contaCorrente.cliente.nome = "Leandro";
+		//contaCorrente.cliente.idade = 25;
+		
+		//contaPoupanca.numero = 22;
+		//contaPoupanca.saldo = 30;
+		//contaPoupanca.cliente.nome = "Santiago";
+		//contaPoupanca.cliente.idade = 40;
+		
+		//contaInvestimento.numero = 33;
+		//contaInvestimento.saldo = 40;
+		//contaInvestimento.cliente.nome = "Ciclano";
+		//contaInvestimento.cliente.idade = 40;
+		
+		//System.out.println(contaCorrente.numero);
+		//System.out.println(contaCorrente.saldo);
+		//System.out.println(contaCorrente.cliente.nome);
+		//System.out.println(contaCorrente.cliente.idade);
+		
 	}
 
 }
