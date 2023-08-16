3c921761ae25c5b8f4016b6e586fee1c1ad03d44
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-08-16 20:12:55 -0300

    Update TesteArquivo.java

--- TesteArquivo/src/TesteArquivo.java
+++ TesteArquivo/src/TesteArquivo.java
@@ -1,4 +1,21 @@
+import java.util.List;
 
 public class TesteArquivo {
+		
+	public static void main (String[] args) {
+		
+		String nomeDoArquivo = "estoque.csv";
+		String diretorio = System.getProperty("user.home");
+		String caminho = diretorio +"\\"+ nomeDoArquivo;
+		
+		List<String>conteudo = new ArrayList<>();
+		conteudo.add("Produto; Quantidade ; Unidade de Medida; Valor da Unidade;" );
+		conteudo.add("Pera; 200; pct; R$5,40;");
+		conteudo.add("Morango; 150; pct; R$6,40;");
+		conteudo.add("Abacaxi; 220; pct; R$4,40;");
+		
+		gravarEstoque(nomeDoArquivo, caminho, conteudo);
+		//letEstoque(caminho)
+	}
 
 }
